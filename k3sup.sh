ceb44bba611e20d29308007f43f713da00c9accf
Dan POP <dan.papandrea@sysdig.com>
2020-09-12 23:45:12 -0400

    Update k3sup.sh
    remove default IP's

--- k3sup.sh
+++ k3sup.sh
@@ -1,7 +1,7 @@
 #POPS k3sup script... remember to change the IP's/HOSTNAMES below!
-export sysdig1=35.192.33.199
-export sysdig2=104.198.153.82
-export sysdig3=34.122.126.98
+export sysdig1=
+export sysdig2=
+export sysdig3=
 
 k3sup install --ip $sysdig1  --context k3s --ssh-key ~/.ssh/google_compute_engine --user $(whoami)
 k3sup join --ip $sysdig2 --server-ip $sysdig1  --ssh-key ~/.ssh/google_compute_engine --user $(whoami)

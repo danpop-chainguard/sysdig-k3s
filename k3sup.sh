5caf44d543b0368fe1c710ccc61b1dc64fedb266
Dan POP <dan.papandrea@sysdig.com>
2020-09-28 14:12:39 -0400

    Update k3sup.sh

--- k3sup.sh
+++ k3sup.sh
@@ -1,4 +1,4 @@
-#POPS k3sup script... remember to change the IP's/HOSTNAMES below!
+#POPS k3sup script... remember to change the IP's/HOSTNAMES/Keys below in the export(s) and script in order to point to your newly created instances and your own keys (the below are examples from GCP)
 export sysdig1=
 export sysdig2=
 export sysdig3=
